4aa775316a70120ca286599f98f7696265ce6f03
wvitorsj <74933042+wvitorsj@users.noreply.github.com>
2022-09-20 13:24:54 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
-chmod +x stop.sh
-sh stop.sh
+#chmod +x stop.sh
+#sh stop.sh
 
-nohup ./start.sh  > /tmp/test.out 2>&1 &
+#nohup ./start.sh  > /tmp/test.out 2>&1 &
 
-echo ":::Aguartando start Java:::"
-sleep 5
+#echo ":::Aguartando start Java:::"
+#sleep 5
 mvn test
 
-sh stop.sh
+#sh stop.sh
