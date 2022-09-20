551b5720d72a2fb0f0ff9133bf6b3b91b919ee5b
Welton Vitor <wvitorsj08@gmail.com>
2022-09-20 14:26:27 -0300

    Atualizando deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-sed -i '' -e "s/IMG_PARA_SUBSTITUIR/wviyotsj\/ecommerce-ibm:$TAG/" docker-compose.yml
+sed -i '' -e "s/IMG_PARA_SUBSTITUIR/wviyotsj\/hellobank-ibm:$TAG/" docker-compose.yml
 docker-compose down 
 docker-compose up 
