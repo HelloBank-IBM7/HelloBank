d86ecbb995e532d090bc7d25e8fb9e2db94c8078
Welton Vitor <wvitorsj08@gmail.com>
2022-09-20 14:45:39 -0300

    Atualizando deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker image rm wviyotsj/hellobank-ibm:$TAG
-sed -i '' -e "s/IMG_PARA_SUBSTITUIR/wviyotsj\/hellobank-ibm:$TAG/" docker-compose.yml
+sed -i '' -e "s/IMG_PARA_SUBSTITUIR/wvitorsj\/hellobank-ibm:$TAG/" docker-compose.yml
 docker-compose down 
 docker-compose up 
